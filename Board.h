d91e3b49879d1436ade3e5182a1326d63787c780
Ben <c.ben.pratt@gmail.com>
2024-04-16 23:51:59 -0400

    this too

--- Board.h
+++ Board.h
@@ -167,4 +167,13 @@ void calculateNearbyBombs(){
         board[i][j]->backToNormal();
 
     }
+    void reset(){
+        for(int i = 0; i < numOfRows; i++){
+            for(int j = 0; j<numOfCols; j++){
+                board[i][j] = new Tile(i*32, j*32, "../Project 3 - Minesweeper Spring 2024/files/images/tile_hidden.png");
+            }
+        }
+        setBombs();
+        calculateNearbyBombs();
+    }
 };
